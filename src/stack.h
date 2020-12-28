3c0ad751272e46bbcb821bbb0ea0de746a036a41
wojciechkepka <wojtek.kepka@protonmail.com>
2020-12-28 19:13:57 +0100

    Add a stack structure

---
+++ src/stack.h
@@ -0,0 +1,23 @@
+#ifndef CSH_STACKH
+#define CSH_STACKH
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+typedef struct
+{
+    char **items;
+    int top;
+    int capacity;
+} sstack_t;
+
+sstack_t *stack_init(size_t capacity);
+void stack_free(sstack_t *stack);
+bool stack_is_full(sstack_t *stack);
+bool stack_is_empty(sstack_t *stack);
+void stack_push(sstack_t *stack, char* item);
+char *stack_pop(sstack_t *stack);
+
+
+#endif // CSH_STACKH
