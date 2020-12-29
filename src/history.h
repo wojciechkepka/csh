3f23f5c85d0b97499eb43cb201fffa5e072f1685
Wojciech Kępka <wojtek.kepka@protonmail.com>
2020-12-29 14:54:59 +0100

    Moar comments

--- src/history.h
+++ src/history.h
@@ -6,6 +6,13 @@
 #include <string.h>
 
 
+/* automatically growing data structure with ability to continiously add
+ * new elements to the end
+ * 
+ * @field elems inner array of elements
+ * @field capacity current capacity of elems
+ * @field back current position in elems
+ */
 typedef struct
 {
     char **elems;
@@ -14,9 +21,34 @@ typedef struct
 } history_t;
 
 
+/* initializes a history instance with specified capacity. 
+ *
+ * @param capacity base capacity
+ * @returns a pointer to history instance or null if allocations failed
+ */
 history_t *history_init(size_t capacity);
+
+/* checks whether maximum capacity has been reached
+ *
+ * @param hist a pointer to history instance
+ * @returns true if hist is full and false otherwise
+ */
 bool history_is_full(history_t *hist);
+
+/* adds a element to history by allocating space and copying its content. 
+ * If number of elements exceeds capacity realloc is called to allocate
+ * twice as much space.
+ * 
+ * @param hist a pointer to history instance
+ * @param item an item to add to hist
+ * @returns 0 if operation succeded, -1 if allocation or reallocation failed
+ */
 int history_add(history_t *hist, char *item);
+
+/* frees up memory of all components of history instance
+ *
+ * @param hist a pointer to history instance
+ */
 void history_free(history_t *hist);
 
 
